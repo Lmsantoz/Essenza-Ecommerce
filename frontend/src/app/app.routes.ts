ee62451e4906b6dbf341a9efa01539e1fb02350a
Lucas Marques <lucaamarques2406@gmail.com>
2025-11-21 10:52:05 -0300

    configurando rota para tela de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { Login } from './components/login/login';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', component:Login,
+    },
+    {
+        path: '', redirectTo:"login", pathMatch: 'full'
+    },
+    {
+        path: '**', component:Login
+    }
+];
