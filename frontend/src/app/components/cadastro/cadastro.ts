7ddc1789241df7e99f21ae4bf7e6ba30b0c91f0a
Lucas Marques <lucaamarques2406@gmail.com>
2025-11-22 07:40:43 -0300

    estilizando pagina de cadastro

--- frontend/src/app/components/cadastro/cadastro.ts
+++ frontend/src/app/components/cadastro/cadastro.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
+  standalone:true,
 })
 export class Cadastro {
 
